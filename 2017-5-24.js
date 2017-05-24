5909b199309dadaa3e1860898040859834500b76
KKTRKKT <paulhana6006@gmail.com>
2017-05-24 22:48:27 +0900

    HTML 더 학습하기, 자바 스크립트
    도형과 색칠

---
+++ 2017-5-24.js
@@ -0,0 +1,18 @@
+// JavaScript source code
+
+// 큰 도형을 기준으로 내림차 순
+noStroke();// 도형의 선을 제거시킴
+ellipse(200, 200, 200, 200); //원
+rect(76, 45, 250, 300); // face 네모
+
+rect(126, 250, 152, 60); // mouth
+
+rect(140, 150, 30, 30); // left eye
+
+rect(240, 150, 30, 30); // right eye
+
+rect(175, 345, 50, 50); // neck
+stroke(191, 0, 255); //선 색
+strokeWeight(37);   //선 두께
+// unibrow
+line(140, 129, 270, 129); //줄 
