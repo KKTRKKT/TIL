21107a3c82883c35448963dd206f3d6faa844394
KKTRKKT <paulhana6006@gmail.com>
2017-05-28 22:56:52 +0900

    while, for 반복문 + image 함수 ,
    while () {} for (;;) {}

---
+++ 2017-5-28(2).js
@@ -0,0 +1,13 @@
+// JavaScript source code
+imageMode(CENTER);
+
+
+for (var j = 0; j < 13; j++) {
+    fill(255, 0, 0);
+    text(j, 13, j * 30 + 30);
+    for (var i = 0; i < 12; i++) { //for 문 내포 j가 한번 돌떄마다 i는 다 돌음
+        fill(130, 56, 56);
+        text(i, i * 36, j * 30 + 30);
+        image(getImage("cute/Heart"), i * 36, j * 30, 40, 60);   
+    }
+}
