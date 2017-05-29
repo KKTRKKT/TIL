8ed9a67e3c0f75fad4d3eb96348f04a2f9678bda
KKTRKKT <paulhana6006@gmail.com>
2017-05-29 22:56:27 +0900

    배열 활용, 함수 호출, 비
    var arrary [{}];

---
+++ 2017-5-29(3).js
@@ -0,0 +1,17 @@
+// JavaScript source code
+var paintBrush = {
+    x: 164,
+    y: 154,
+    img: getImage("avatars/leaf-grey")
+};
+
+var paintCanvas = function () {
+    imageMode(CENTER);
+    image(paintBrush.img, paintBrush.x, paintBrush.y);
+};
+mouseMoved = function () {
+    paintBrush.x = (mouseX);
+    paintBrush.y = (mouseY);
+    paintCanvas();
+};
+
---
+++ 비.js
@@ -0,0 +1,20 @@
+// JavaScript source code
+var xPositions = [200];
+var yPositions = [0];
+
+draw = function () {
+    background(204, 247, 255);
+
+    xPositions.push(random(0, 400));
+    yPositions.push(0);
+
+    image(getImage("avatars/aqualine-sapling"), 211, 278);
+
+    for (var i = 0; i < xPositions.length; i++) {
+        noStroke();
+        fill(0, 200, 255);
+        ellipse(xPositions[i], yPositions[i], 2, 26);
+        yPositions[i] += 5;
+    }
+
+};
