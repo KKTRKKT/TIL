c707209ecb548eddf4b00fdc1841e6bccd5614bc
KKTRKKT <paulhana6006@gmail.com>
2017-06-08 22:43:21 +0900

    각도
    도형 각도 translate(x, y), rotate(각도);

---
+++ 2017-6-8(2).js
@@ -0,0 +1,14 @@
+// JavaScript source code
+background(255, 255, 255);
+
+stroke(120, 120, 120);
+fill(0, 0, 255, 100);
+rect(20, 20, 40, 40);
+
+pushMatrix();
+translate(20, 20);
+scale(2.0);
+fill(255, 0, 0, 100);
+stroke(0, 0, 0);
+rect(0, 0, 40, 40);
+popMatrix();
---
+++ 2017-6-8.js
@@ -0,0 +1,22 @@
+// JavaScript source code
+noStroke();
+background(0, 0, 0);
+var r = 255;
+var g = 90;
+var b = 0;
+fill(r, g, b);
+
+
+for (var i = 0; i < 12; i++) {
+    pushMatrix();
+    translate(200, 200);
+    r -= i * 6;
+    g += i * 4;
+    b += i * 6;
+    fill(r, g, b);
+    rotate(i * 30);
+
+    rect(0, 0, 150, 15);
+
+    popMatrix();
+}
